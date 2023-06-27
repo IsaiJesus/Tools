15c2a90f13527843131147ad7496af1a28810b43
Isai Jesus <isaijesus02@gmail.com>
2023-06-27 16:12:18 -0600

    Showing URI

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -7,7 +7,7 @@ const conn = {
 export async function dbConnect() {
   if (conn.isConnected) return;
 
-  const db = await connect(process.env.MONGODB_URL);
+  const db = await connect("mongodb+srv://Isai:isai23_@cluster0.mo5xfu3.mongodb.net/tools?retryWrites=true&w=majority");
   conn.isConnected = db.connections[0].readyState;
 
   console.log(db.connection.db.databaseName);
