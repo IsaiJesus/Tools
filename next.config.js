eafd52de6da5eae376c21c3df1bbc2854bfb51a5
Isai Jesus <isaijesus02@gmail.com>
2023-06-26 20:50:04 -0600

    Fixing envs 2

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, { webpack }) => {
     config.plugins.push(new webpack.EnvironmentPlugin(process.env));
     return config;
   },
-}
-
-module.exports = nextConfig
+};
